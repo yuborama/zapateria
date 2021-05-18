0f7b59cf086512129a86030df4526822dffaa8b0
Yuborama <davidjohan2@hotmail.com>
2021-05-18 01:49:45 -0500

    modific texts add margin paddig fontfamyly fontweight

--- src/components/atoms/AtomTextTittle/index.tsx
+++ src/components/atoms/AtomTextTittle/index.tsx
@@ -5,13 +5,18 @@ type texttittleProps = {
   size?: string;
   color?: string;
   as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
-  fweight?:"bold" |"normal"
+  fontweight?: "Thin" |"Extra-ligth" | "Ligth" | "Regular" | "Medium" | "Semi-bold" | "Bold" | "Extra-bold"| "Black"
+  margin?: string;
+  padding?: string;
 };
 
 const Component = styled.h1<texttittleProps>` 
+  font-family:  'Inter', sans-serif;
   font-size: ${({ size }) => size || "44px"};
   color: ${({ color }) => color || "#3C3E42"};
-  font-weight:${({fweight}) => fweight || "normal"} ;
+  margin: ${({ margin }) => margin || "0px"};
+  padding: ${({ padding }) => padding || "0px"};
+  font-weight:${({fontweight}) => fontweight || "Thin"} ;
 `;
 
 const AtomTextTittle: FC<texttittleProps> = (props) => {
