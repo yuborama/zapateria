291e5bcdeab763434aa24e95d9b7d4bc40667ceb
Yuborama <davidjohan2@hotmail.com>
2021-05-23 16:48:32 -0500

    Add: MoleculeNavigation

---
+++ src/components/molecules/MoleculeNavigation/styled.ts
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled"
+import Link from "next/Link";
+export const Nav = styled.nav`
+top: 0;
+width: 100vw;
+position: fixed;
+z-index: 1000;
+`;
+
+export const Linkstyled = styled(Link)` 
+        text-decoration: none;
+    a{ 
+        text-decoration: none;
+    }
+`;
