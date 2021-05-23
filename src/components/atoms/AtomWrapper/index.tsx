03a82772b70003c49294ff00304d71eca88d0167
Yuborama <davidjohan2@hotmail.com>
2021-05-23 16:53:38 -0500

    Add: Grid, Gap, Id AtomWrapper

--- src/components/atoms/AtomWrapper/index.tsx
+++ src/components/atoms/AtomWrapper/index.tsx
@@ -11,25 +11,31 @@ type AtomWrapperProps = {
     | "space-around"
     | "space-evenly";
   fullwidth?: boolean;
+  display?: "grid" | "flex";
+  columns?: number;
   flexDirection?: "row" | "column" | "row-reverse" | "column-reverse";
   backgroudColor?: string;
   margin?: string;
   padding?: string;
   width?: string;
   height?: string;
+  gap?: string;
+  id?: string;
 };
 
 const Container = styled.div<AtomWrapperProps>`
-  height: ${({ height }) => (height || "100%")};
-  width: ${({ width }) => (width || "100%")};
-  display: flex;
-  position: relative;   
-  margin: ${({ margin }) => (margin || "0px")};
+  position: relative;
+  height: ${({ height }) => height || "100%"};
+  width: ${({ width }) => width || "100%"};
+  display: ${({ display }) => display || "flex"};
+  grid-template-columns: repeat(${({ columns }) => columns || 1}, 1fr);
+  margin: ${({ margin }) => margin || "0px"};
   padding: ${({ padding }) => padding || "0px"};
   flex-direction: ${({ flexDirection }) => flexDirection || "row"};
   align-items: ${({ alignItems }) => alignItems || "flex-start"};
   justify-content: ${({ justifyContent }) => justifyContent || "flex-start"};
   background-color: ${({ backgroudColor }) => backgroudColor || "transparent"};
+  gap:  ${({ gap }) => gap || "0px"};
 `;
 
 const AtomWrapper: FC<AtomWrapperProps> = (props) => {
